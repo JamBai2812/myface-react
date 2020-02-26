651eddc09e24c10b5ac838c72957a40d827f5fc5
Jamie Baird <james.baird@softwire.com>
2020-02-26 16:30:00 +0000

    Refactored to add extra component for Post and PostList

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,42 +1,19 @@
-﻿import React, {useState, useEffect} from "react";
+﻿import React,{useState, useEffect} from "react";
 
-function Post() {
-
-    const [postData, setPostData] = useState(null);
-
-    useEffect(() => {
-        fetch("https://localhost:5001/posts")
-            .then(response => response.json())
-            .then(response => setPostData(response.items))
-    }, []);
-
-    if (!postData) {
-        return <div>waiting for data......</div>
-    }
+function Post(props) {
+    
     
     return (
-        <ul className="postList">
-            {postData.map(post =>
-                <li>
-                    <h2>{post.postedBy.firstName}</h2>
-                    <img className="userImage"
-                         src={post.postedBy.profileImageUrl}/>
-                    <p>{post.message}</p>
-                    <img className="postImage" src={post.imageUrl} />
-                    <p>Date posted: {}</p>
-                    <p>Time posted: </p>
-                </li>
-            )}
-            
-
-            
-
-        </ul>
-
-
+        <li className="postContents">
+            <h2>{props.data.postedBy.firstName}</h2>
+            <img className="userImage"
+                 src={props.data.postedBy.profileImageUrl}/>
+            <p>{props.data.message}</p>
+            <img className="postImage" src={props.data.imageUrl} />
+            <p>Date posted: </p>
+            <p>Time posted: </p>
+        </li>
     );
-
-
 }
 
 export {Post};
--- src/Components/PostList.js
+++ src/Components/PostList.js
@@ -1,12 +1,31 @@
-﻿import React from "react";
+﻿import React, {useState, useEffect} from "react";
 import {Post} from "./Post";
 
 function PostList() {
-    
+
+    const [postData, setPostData] = useState(null);
+
+    useEffect(() => {
+        fetch("https://localhost:5001/posts")
+            .then(response => response.json())
+            .then(response => setPostData(response.items))
+    }, []);
+
+    if (!postData) {
+        return <div>waiting for data......</div>
+    }
     
     return (
-        <Post/>
+        <ul className="postList">
+            {postData.map(post =>
+                <Post data={post}/>
+            )}
+        </ul>
+
+
     );
+
+
 }
 
 export {PostList};
