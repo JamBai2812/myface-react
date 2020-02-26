172639b157d635187dbf70372d2b479f32562feb
Jamie Baird <james.baird@softwire.com>
2020-02-26 16:10:55 +0000

    Used state and useEffect to get data from api and put into posts

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,28 +1,42 @@
 ﻿import React, {useState, useEffect} from "react";
 
 function Post() {
-    
-    // const [postData, setPostData] = useState(null);
-    //
-    // useEffect(()=> {
-    //     fetch()
-    //         .then(response => setPostData(response.data))
-    // }, []);
-    
+
+    const [postData, setPostData] = useState(null);
+
+    useEffect(() => {
+        fetch("https://localhost:5001/posts")
+            .then(response => response.json())
+            .then(response => setPostData(response.items))
+    }, []);
+
+    if (!postData) {
+        return <div>waiting for data......</div>
+    }
     
     return (
-        <li className="post">
-            <div className="postContents">
-                <h2>Linda</h2>
-                <img className="userImage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Terrier_mixed-breed_dog.jpg/220px-Terrier_mixed-breed_dog.jpg"/>
-                <p>My sunset picture</p>
-                <img className="postImage" src="https://upload.wikimedia.org/wikipedia/commons/9/90/Sunset_Marina.JPG" />
-                <p>Date posted: </p>
-                <p>Time posted: </p>
-            </div>
+        <ul className="postList">
+            {postData.map(post =>
+                <li>
+                    <h2>{post.postedBy.firstName}</h2>
+                    <img className="userImage"
+                         src={post.postedBy.profileImageUrl}/>
+                    <p>{post.message}</p>
+                    <img className="postImage" src={post.imageUrl} />
+                    <p>Date posted: {}</p>
+                    <p>Time posted: </p>
+                </li>
+            )}
+            
 
-        </li>
+            
+
+        </ul>
+
+
     );
+
+
 }
 
 export {Post};
--- src/Components/PostList.js
+++ src/Components/PostList.js
@@ -2,18 +2,10 @@
 import {Post} from "./Post";
 
 function PostList() {
-
-    const postList = [];
-        for(let i = 0; i<5; i++){
-            
-            postList.push(<Post/>);
-        }
     
     
     return (
-        <ul className="postList">
-            {postList}
-        </ul>
+        <Post/>
     );
 }
 
