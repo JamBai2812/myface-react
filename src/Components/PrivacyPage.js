4ef0eaf7b5efe9ea1bb005f5d62aa75e3ba7c7b1
Jamie Baird <james.baird@softwire.com>
2020-02-27 12:41:12 +0000

    Brought nav bar to front and changed privacy page

--- src/Components/PrivacyPage.js
+++ src/Components/PrivacyPage.js
@@ -2,9 +2,11 @@
 
 function PrivacyPage() {
     return (
-        <main>
-            <h1>Privacy</h1>
-            <p>Your data is safe with us....</p>
+        <main className="privacyText">
+            <div >
+                <h1>Privacy</h1>
+                <p>Your data is safe with us....</p>
+            </div>
         </main>
     );
 }
