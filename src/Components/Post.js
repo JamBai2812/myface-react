2e6355f33cbb45955147a733cf148c65d7ad39e5
Jamie Baird <james.baird@softwire.com>
2020-02-27 10:59:37 +0000

    Added more things to post.
    Added like/dislike counts (no functionality yet) and used moments to display date and time posted.

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,17 +1,24 @@
-﻿import React,{useState, useEffect} from "react";
+﻿import React from "react";
+import moment from "moment";
 
 function Post(props) {
+    
+    var postDateTime = moment(props.data.postedAt);
+    var postDate = postDateTime.format("ddd Do MMM YYYY");
+    var postTime = postDateTime.format("h:mm a");
+    
     
     
     return (
         <li className="postContents">
             <h2>{props.data.postedBy.firstName}</h2>
-            <img className="userImage"
-                 src={props.data.postedBy.profileImageUrl}/>
+            <img className="userImage" src={props.data.postedBy.profileImageUrl} alt={props.data.postedBy.displayName + " profile picture."}/>
             <p>{props.data.message}</p>
-            <img className="postImage" src={props.data.imageUrl} />
-            <p>Date posted: </p>
-            <p>Time posted: </p>
+            <img className="postImage" src={props.data.imageUrl} alt="Image posted."/>
+            <p className="dateTimePosted"><b>Posted:</b> {postDate}<br/>
+            at &nbsp;{postTime}
+            </p>
+            <p className="likesDislikes"><b>Likes:</b> {props.data.likes.length} &emsp; <b>Dislikes:</b> {props.data.dislikes.length}</p>
         </li>
     );
 }
