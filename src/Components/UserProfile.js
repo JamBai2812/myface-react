95fd21939cff8b52740c49f60945888750e4d7d5
Jamie Baird <james.baird@softwire.com>
2020-02-28 10:13:04 +0000

    Changed style of users page

--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -2,15 +2,26 @@
 
 function UserProfile(props) {
 
+    const divStyle = 
+        {
+                        'width': '100%',
+                        'background-image': 'url(' + props.data.coverImageUrl + ')',
+                        'background-size': "cover",
+                        'border-radius': '5%'
+        };
+
+
 
     return (
-        <li className="postContents">
-            <div style={{"background-image": props.data.coverImageUrl}}><h2>{props.data.displayName}</h2></div>
-            <img className="postImage" src={props.data.coverImageUrl} alt="Image posted."/>
+        <li className="userProfileContents">
+            <div style={divStyle}>
+                <h2 className="userProfileName">{props.data.displayName}<br/></h2>
+                <img className="userImage" src={props.data.profileImageUrl}
+                     alt={props.data.displayName + " profile picture."}/>
+            </div>
+            <p className="userProfileUsername"><b>Username:</b> {props.data.username}</p>
+            <p className="userProfileEmail"><b>Email:</b> {props.data.email}</p>
             
-            <img className="userImage" src={props.data.profileImageUrl} alt={props.data.displayName + " profile picture."}/>
-            <p className="postMessage">{props.data.email}</p>
-            <p className="userName">{props.data.username}</p>
         </li>
     );
 }
