3dcb448320e0b81ee268db888489302128695769
Jamie Baird <james.baird@softwire.com>
2020-02-28 14:43:36 +0000

    Made more responsive and added modal image

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -7,18 +7,35 @@ function Post(props) {
     const postDateTime = moment(props.data.postedAt);
     const postDate = postDateTime.format("ddd Do MMM YYYY");
     const postTime = postDateTime.format("h:mm a");
-
-
+    
+    
+    
+    function openModal(){
+        const modal = document.getElementById(props.data.imageUrl +"modal");
+        modal.style.display = "block";
+    }
+    
+    function closeModal(){
+        const modal = document.getElementById(props.data.imageUrl +"modal");
+        modal.style.display = "none";
+    }
+    
+    
     return (
         <li className="postContents">
-            <h2>{props.data.postedBy.firstName}</h2>
+            <h2 className="postedBy">{props.data.postedBy.firstName}</h2>
             <img className="userImage" src={props.data.postedBy.profileImageUrl} alt={props.data.postedBy.displayName + " profile picture."}/>
             <p className="postMessage">{props.data.message}</p>
-            <img className="postImage" src={props.data.imageUrl} alt="Image posted."/>
+            <img onClick={openModal} id={props.data.imageUrl} className="postImage" src={props.data.imageUrl} alt="Image posted."/>
             <p className="dateTimePosted"><b>Posted:</b> {postDate}<br/>
             at &nbsp;{postTime}
             </p>
             <p className="likesDislikes"><b>Likes:</b> {props.data.likes.length} &emsp; <b>Dislikes:</b> {props.data.dislikes.length}</p>
+            <div id={props.data.imageUrl +"modal"} className="modal">
+                <span onClick={closeModal} className="close">&times;</span>
+                <img id={props.data.imageUrl +"imgModal"} className="modalImage" src={props.data.imageUrl}/>
+                <div className="caption" id={props.data.message}>{props.data.message}</div>
+            </div>
         </li>
     );
 }
--- src/Components/PostList/PostList.js
+++ src/Components/PostList/PostList.js
@@ -1,10 +1,13 @@
 ﻿﻿import React, {useState, useEffect} from "react";
 import {Post} from "../Post/Post";
 import './PostList.scss';
+import {FaArrowLeft, FaArrowRight} from 'react-icons/fa';
 
 function PostList(props) {
 
     const [postData, setPostData] = useState(null);
+    const [nextPage, setNextPage] = useState("");
+    const [previousPage, setPreviousPage] = useState("");
 
     useEffect(() => {
         fetch("https://localhost:5001/posts")
@@ -16,12 +19,50 @@ function PostList(props) {
         return <div>waiting for data......</div>
     }
 
+    function handleNextPageButtonPress(){
+        if(nextPage){
+            fetch("https://localhost:5001"+nextPage)
+                .then(response => response.json())
+                .then(response =>
+                {
+                    setPostData(response.items);
+                    setNextPage(response.nextPage);
+                    setPreviousPage(response.previousPage);
+
+                })
+        }
+
+    }
+
+    function handlePreviousPageButtonPress(){
+        if(previousPage){
+            fetch("https://localhost:5001"+previousPage)
+                .then(response => response.json())
+                .then(response =>
+                {
+                    setPostData(response.items);
+                    setNextPage(response.nextPage);
+                    setPreviousPage(response.previousPage);
+
+                })
+        }
+    }
+
+
     return (
-        <ul className="postList">
-            {postData.map(post =>
-                <Post data={post}/>
-            )}
-        </ul>
+        <div>
+            <ul className="postList">
+                {postData.map(post =>
+                    <Post data={post}/>
+                )}
+            </ul>
+            <button className="changePage" onClick={handlePreviousPageButtonPress}><FaArrowLeft /></button>
+            &nbsp;&nbsp;&nbsp;
+            <button className="changePage" onClick={handleNextPageButtonPress}><FaArrowRight/></button> 
+            
+        </div>
+       
+        
     );
 }
 
